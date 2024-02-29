a876995fb9c4e15a0d552cb1c56c84600a6cdd86
Akiva3982432849 <78503106+Akiva3982432849@users.noreply.github.com>
2024-02-29 09:22:08 -0500

    Thursday work

--- src/App.java
+++ src/App.java
@@ -1,7 +1,40 @@
 import processing.core.*;
 
 public class App extends PApplet {
-    public static void main(String[] args)  {
+    Box myBox;
+
+    public static void main(String[] args) {
         PApplet.main("App");
+    }
+
+    public void settings() {
+        size(1000, 600);
+    }
+
+    public void setup() {
+        background(255);
+        myBox = new Box(50, 500, 100, 50); // Specify width and height for the box
+    }
+
+    public void draw() {
+        background(255);
+        myBox.display();
+    }
+
+    class Box {
+        float x, y;
+        float boxWidth, boxHeight; // Separate variables for width and height
+
+        Box(float x, float y, float boxWidth, float boxHeight) {
+            this.x = x;
+            this.y = y;
+            this.boxWidth = boxWidth;
+            this.boxHeight = boxHeight;
+        }
+
+        void display() {
+            fill(0);
+            rect(x, y, boxWidth, boxHeight);
+        }
     }
 }
